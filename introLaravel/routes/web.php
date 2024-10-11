0f6ce50e02f08a722feb107fd77516f30b2fc47a
joaco2752 <122044319@upq.edu.mx>
2024-10-10 19:01:27 -0600

    practica 4

--- introLaravel/routes/web.php
+++ introLaravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorVistas;
 
 /* Route::get('/', function () {
     return view(view: 'inicio');
@@ -14,7 +15,11 @@ Route::get('/consultar', function () {
     return view(view: 'clientes');
 })->name('rutaClientes'); */
 
-Route::view('/','inicio')->name('rutaInicio');
+/* Route::view('/','inicio')->name('rutaInicio');
 Route::view('/form','formulario')->name('rutaFormulario');
 Route::view('/consultar','clientes')->name('rutaClientes');
-Route::view('/component','componentes')->name('rutaComponent');
+Route::view('/component','componentes')->name('rutaComponent'); */
+
+Route::get('/',[ControladorVistas::class, 'home'])->name('rutaInicio');
+Route::get('/form',[ControladorVistas::class, 'formulario'])->name('rutaFormulario');
+Route::get('/consultar',[ControladorVistas::class, 'consulta'])->name('rutaClientes');
