9003433729512bf81ec542fdf06ec35e5f73eb95
joaco2752 <122044319@upq.edu.mx>
2024-11-05 22:05:08 -0600

    segundo commit examen

--- examenSegundoParcial/resources/views/registro.blade.php
+++ examenSegundoParcial/resources/views/registro.blade.php
@@ -7,41 +7,33 @@
     <title>Registro</title>
 </head>
 <body>
+    <div class="container card-monospace">
     <div class="card-header fs-5 text-center text-primary">
         Registro de Prendas
     </div>
 
-    @session('exito')
-        Swal.fire({
-        title: "Respuesta Servidor",
-        text: "{{$value}}",
-        icon: "success"});
-        </script>
-        @endsession
-
     <div class="card body text-justify">
         <form action="/registrarPrenda" method="POST">
         @csrf
-            <div class="container mt-4">
             <div class="mb-3">
                 <label for="prenda" class="form-label">Prenda</label>
-                <input type="text" class="form-control" name="txtprenda">
+                <input type="text" class="form-control" name="txtprenda" value="{{old('txtprenda')}}">
             </div>
 
             <div class="mb-3">
                 <label for="color" class="form-label">Color</label>
-                <input type="text" class="form-control" name="txtcolor">
+                <input type="text" class="form-control" name="txtcolor" value="{{old('txtcolor')}}">
             </div>
 
             <div class="mb-3">
                 <label for="cantidad" class="form-label">Cantidad</label>
-                <input type="text" class="form-control" name="txtcantidad">
+                <input type="text" class="form-control" name="txtcantidad" value="{{old('txtcantidad')}}">
             </div>
 
-            <button type="submit" class="btn btn-primary">Guardar Prendas</button>
+            <div class="d-inline-flex gap-1">
+            <button type="submit" class="btn btn-primary">Guardar Prenda</button>
             </div>
         </form>
-    </div>
     </div>
     </div>
 </body>
--- examenSegundoParcial/routes/web.php
+++ examenSegundoParcial/routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controlador;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('registro');
-});
+}); */
 
-Route::get('/', [Controlador::class, 'home'])->name('registrar');
+Route::get('/', [Controlador::class, 'home'])->name('rutaRegistrar');
 Route::get('/registrarPrenda', [Controlador::class, 'procesarPrenda'])->name('rutaRegistro');
 Route::post('/registrarPrenda', [Controlador::class, 'procesarPrenda'])->name('rutaRegistro');
